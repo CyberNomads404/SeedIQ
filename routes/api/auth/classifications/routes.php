cc2bad69583d51f06d341968c09ae78dca83445e
thayna-bezerra <arraisthayna2106@gmail.com>
2025-11-22 18:46:34 -0300

    feat: adicionar rota de reanálise para classificações, permitindo o reenvio para análise

--- routes/api/auth/classifications/routes.php
+++ routes/api/auth/classifications/routes.php
@@ -6,4 +6,5 @@ Route::prefix('classifications')->group(function () {
     Route::get('/', [ClassificationController::class, 'index'])->middleware('permission:classifications_list')->name('classifications.api.index');
     Route::post('/', [ClassificationController::class, 'store'])->middleware('permission:classifications_create')->name('classifications.api.store');
     Route::get('/{externalId}', [ClassificationController::class, 'show'])->middleware('permission:classifications_list')->name('classifications.api.show');
+    Route::post('/{externalId}/reanalyze', [ClassificationController::class, 'reanalyze'])->middleware('permission:classifications_update')->name('classifications.api.reanalyze');
 });
