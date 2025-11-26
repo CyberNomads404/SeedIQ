807f9a66128324ac7dd1ec35b7511083f1eb0197
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2025-11-26 02:13:31 -0300

    fix: corrigir nome da rota para webhook.analyze.set_result

--- routes/api/webhook/analyze/routes.php
+++ routes/api/webhook/analyze/routes.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\Classification\ClassificationController;
 
 Route::prefix('/analyze')->middleware('validate.api.token:seediq-ai')->group(function () {
-    Route::post('/', [ClassificationController::class, 'setResult'])->name('webhook.analyze.setResult');
+    Route::post('/', [ClassificationController::class, 'setResult'])->name('webhook.analyze.set_result');
 });
 
